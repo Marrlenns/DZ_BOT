7fcaf9a486c418cb5e662acf33bf4bc0a327be4d
Marrlenns <marlen.ormonbaev05@gmail.com>
2022-07-20 17:59:34 +0600

    first

--- handlers/extra.py
+++ handlers/extra.py
@@ -2,12 +2,12 @@ from aiogram import types, Dispatcher
 from config import bot, ADMIN
 import random
 
+
 async def game(message: types.Message):
-    if message.text.startswith('game'):
-        if message.from_user.id in ADMIN:
-            emojies = ['🎯', '🎳', '🎰', '🎲', '⚽', '️🏀']
-            rand_game = random.choice(emojies)
-            await bot.send_dice(message.chat.id, emoji=rand_game)
+    if message.text.startswith('game') and message.from_user.id in ADMIN:
+        emojis = ['🎯', '🎳', '🎰', '🎲', '⚽', '️🏀']
+        rand_game = random.choice(emojis)
+        await bot.send_dice(message.chat.id, emoji=rand_game)
     else:
         try:
             k = int(message.text)
@@ -15,9 +15,5 @@ async def game(message: types.Message):
         except:
             await bot.send_message(message.chat.id, message.text)
 
-# async def echo(message: types.Message):
-
-
 def register_handlers_extra(dp: Dispatcher):
     dp.register_message_handler(game)
-    # dp.register_message_handler(echo)
--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from aiogram.utils import executor
 from config import dp
 import logging
 
-from handlers import client, extra, callback, admin
+from handlers import client, extra, callback, admin, fsmAdminMenu
 
+fsmAdminMenu.register_handlers_fsmAdminMenu(dp)
 client.register_handlers_client(dp)
-extra.register_handlers_extra(dp)
+# extra.register_handlers_extra(dp)
 callback.register_handlers_callback(dp)
 # admin.register_handlers_admin(dp)
 
