bd925e9058619a6960f9291c1a6debcf70a4b5e2
Marrlenns <marlen.ormonbaev05@gmail.com>
2022-07-29 01:13:56 +0600

    HW_5

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from aiogram.utils import executor
 from config import dp
 import logging
+import asyncio
 
-from handlers import client, extra, callback, admin, fsmAdminMenu
+from handlers import client, extra, callback, admin, fsmAdminMenu, notification
 from database.bot_db import sql_create
 
 async def on_startup(_):
+    asyncio.create_task(notification.scheduler())
     sql_create()
 
+notification.register_handler_notification(dp)
 fsmAdminMenu.register_handlers_fsmAdminMenu(dp)
 client.register_handlers_client(dp)
-# extra.register_handlers_extra(dp)
 callback.register_handlers_callback(dp)
-# admin.register_handlers_admin(dp)
+admin.register_handlers_admin(dp)
+extra.register_handlers_extra(dp)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
