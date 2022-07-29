9319fcb42cac46e3048b6b267b9aa919306cb4eb
Marrlenns <marlen.ormonbaev05@gmail.com>
2022-07-30 01:22:28 +0600

    first commit

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,6 +1,7 @@
 from aiogram import types, Dispatcher
 from config import ADMIN, bot, dp
-from database.bot_db import sql_command_all
+from database.bot_db import sql_commands_get_all_id
+
 
 async def reklama(message: types.Message):
     if message.from_user.id in ADMIN:
---
+++ parser/news.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://www.securitylab.ru/news/"
+
+HEADERS = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+}
+
+def get_html(url, params=''):
+    req = requests.get(url, headers=HEADERS, params=params)
+    return req
+
+print(get_html(URL))
