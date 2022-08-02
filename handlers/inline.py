c30097193d413e477c25ceebc4955dbd63999418
Marrlenns <marlen.ormonbaev05@gmail.com>
2022-08-02 20:06:56 +0600

    love ESEN

--- handlers/inline.py
+++ handlers/inline.py
@@ -52,6 +52,6 @@ async def inline_wikipedia_handler(query: types.InlineQuery):
     await query.answer(articles, cache_time=60, is_personal=True)
 
 def register_inline_handler(dp: Dispatcher):
-    # dp.register_inline_handler(inline_youtube_handler)
-    dp.register_inline_handler(inline_google_handler)
+    dp.register_inline_handler(inline_youtube_handler)
+    # dp.register_inline_handler(inline_google_handler)
     # dp.register_inline_handler(inline_wikipedia_handler)
